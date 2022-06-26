23276c7ad525f38c04533cef19421af4d7916b27
tu <63336646+tumolaha@users.noreply.github.com>
2022-06-26 22:33:31 +0700

    add  customize-cra  and babel-plugin-module-resolver part

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
-/* config-overrides.js */
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
-  }
+const { override,useBabelRc } = require("customize-cra");
+//const path = require("path");
+
+module.exports = override(
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useBabelRc(),
+);
